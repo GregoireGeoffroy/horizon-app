8fdf4d28f5b7bd270ea0f3e2e6d37a971004baf1
Gregoire Geoffroy <thegathering92@gmail.com>
2024-12-05 15:37:25 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'images.unsplash.com',
+      // Add other image domains you'll use
+    ],
+  },
 };
 
 export default nextConfig;
